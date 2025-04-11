3743614126bb55653038b3153226710956b01957
AhmedOmran22 <mamdouhanahamada@gmail.com>
2025-04-11 16:56:48 +0200

    Add ElFahlawy class with hardware specifications and comments

---
+++ lib/el_fahlawy.dart
@@ -0,0 +1,9 @@
+abstract class ElFahlawy {
+  static const intel_Core_i5G14400f = 6800;
+  static const geforce_RTX_4060Ti = 20500;
+  static const ddr5_32GB = 7699;
+  static const ssd_2TB = 7500;
+  static const total = 42600;
+  //? كدا هتعوز تجيبب كيسة وباور سبلاي و كولر
+  //? ممكن يعملو حوالي 8 مثلا
+}
