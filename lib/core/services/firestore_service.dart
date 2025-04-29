437e94d76d336d474b5ca702d9840ec36a0247fb
AhmedOmran22 <mamdouhanahamada@gmail.com>
2025-04-29 14:04:29 +0300

    refactroing the code and delete unused file and delete comments in the firestore file

--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -5,37 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FireStoreService extends ApiService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  // @override
-  // Future<void> addData({
-  //   required String path,
-  //   required Map<String, dynamic> data,
-  //   String? documentId,
-  // }) async {
-  //   if (documentId != null) {
-  //     firestore.collection(path).doc(documentId).set(data);
-  //   } else {
-  //     await firestore.collection(path).add(data);
-  //   }
-  // }
-
-  // // @override
-  // Future<void> updateData({
-  //   required String path,
-  //   required String docuementId,
-  //   required Map<String, dynamic> data,
-  // }) async {
-  //   await firestore.collection(path).doc(docuementId).update(data);
-  // }
-
-  // // @override
-  // Future<dynamic> getData({
-  //   required String path,
-  //   required String docuementId,
-  // }) async {
-  //   var data = await firestore.collection(path).doc(docuementId).get();
-  //   return data.data();
-  // }
-
   @override
   Future delete(
     String path, {
--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -6,12 +6,9 @@ import 'package:blood_bank/features/home/data/repos/community_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import '../../features/auth/data/repos/auth_repo.dart';
-// import '../../features/auth/data/repos/firebase_impl.dart';
 import 'firebase_auth_service.dart';
-// import 'firestore_service.dart';
 
 final getIt = GetIt.instance;
-
 void setupGetit() {
   getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
   getIt.registerSingleton<Dio>(Dio());
