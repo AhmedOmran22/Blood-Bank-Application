140739941716b32f4241e608fcf143243cf366b6
AhmedOmran22 <mamdouhanahamada@gmail.com>
2025-04-30 01:47:00 +0300

    delete static posts and delete from firestore method in user model

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -20,28 +20,7 @@ class UserModel {
     this.gender,
     required this.NationalId,
   });
-  factory UserModel.fromFireStore(Map<String, dynamic> json) => UserModel(
-        uid: json['uid'] as String,
-        email: json['email'] as String,
-        name: json['name'] as String,
-        phoneNumber: json['phoneNumber'] as String,
-        bloodType: json['bloodType'] as String,
-        fcmTokenm: json['fcmToken'] as String,
-        gender: json['gender'] as String,
-        NationalId: json['NationalId'] as String,
-      );
-
-  toMap() => {
-        'uid': uid,
-        'email': email,
-        'name': name,
-        'phoneNumber': phoneNumber,
-        'bloodType': bloodType,
-        'fcmToken': fcmTokenm,
-        'gender': gender,
-        'NationalId': NationalId,
-      };
-
+ 
   toBackendMap(String password) => {
         "email": email,
         "fullName": name,
