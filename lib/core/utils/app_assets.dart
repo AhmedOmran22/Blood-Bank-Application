85f522e0ea5221dcb35473c75886349d5f8b1b09
AhmedOmran22 <mamdouhanahamada@gmail.com>
2025-05-01 14:33:12 +0300

    add new assets for awarness view

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,25 +1,14 @@
-// ignore_for_file: prefer_single_quotes
 class AppAssets {
   AppAssets._();
   
   static const String imagesAnotherTesting = "assets/images/another_testing.svg";
 
-  static const String imagesBloodDrop = "assets/images/blood_drop.svg";
+  static const String imagesAwarness1 = "assets/images/awarness1.jpg";
 
-  static const String imagesBloodDropRed = "assets/images/blood_drop_red.svg";
+  static const String imagesAwarness2 = "assets/images/awarness2.jpg";
 
-  static const String imagesBloodTesting = "assets/images/blood_testing.svg";
+  static const String imagesAwarness3 = "assets/images/awarness3.jpg";
 
-  static const String imagesCalenderDate = "assets/images/calender_date.svg";
-
-  static const String imagesImage1 = "assets/images/image1.png";
-
-  static const String imagesImage2 = "assets/images/image2.png";
-
-  static const String imagesImage3 = "assets/images/image3.png";
-
-  static const String imagesOnBoarding2 = "assets/images/on_boarding2.svg";
-
-  static const String imagesRequestBlood = "assets/images/request_blood.svg";
+  static const String imagesAwarness4 = "assets/images/awarness4.jpg";
 }
 
