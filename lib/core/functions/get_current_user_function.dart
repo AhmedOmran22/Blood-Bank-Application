1218c28c28f507074242d0ff26ab5f91148979c7
AhmedOmran22 <mamdouhanahamada@gmail.com>
2025-05-12 17:57:09 +0300

    Remove commented-out code and fix phoneNumber initialization in getCurrentUserFromBackEndFunction

--- lib/core/functions/get_current_user_function.dart
+++ lib/core/functions/get_current_user_function.dart
@@ -1,21 +1,11 @@
-// import 'dart:convert';
-
 import '../../features/auth/data/models/user_model.dart';
 import '../cache/prefs.dart';
 import '../constants/constatnts.dart';
-
-/// get the data of logged in user !
-// UserModel getCurrentUserFromFirebaseFunction() {
-//   var jsonString = Prefs.getString(kUserData);
-//   var currentUser = UserModel.fromFireStore(jsonDecode(jsonString));
-//   return currentUser;
-// }
-
 UserModel getCurrentUserFromBackEndFunction() {
   UserModel currentUser = UserModel(
     email: Prefs.getString(kUserEmail),
     name: Prefs.getString(kUserName),
-    phoneNumber: 'nope',
+    phoneNumber: '',
     NationalId: Prefs.getString(kUserNationalId),
     bloodType: Prefs.getString(kUserBloodType),
   );
